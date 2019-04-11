d6d92f3586e2d22c250b53bc47fba0ed514c4abd
crh <1148507674@qq.com>
2019-04-11 18:17:11 +0800

    hibernate的实现管理的实现研究

--- aaa/src/main/java/com/hcrh/hibernatedemo/AaaApplication.java
+++ aaa/src/main/java/com/hcrh/hibernatedemo/AaaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
 @SpringBootApplication(scanBasePackages = {"com.hcrh.hibernatedemo.service",
 		"com.hcrh.hibernatedemo.dao"})
 @EntityScan(basePackages = {"com.hcrh.hibernatedemo.entity"})//扫描实体包
@@ -16,5 +17,23 @@ public class AaaApplication {
  	public static void main(String[] args) {
  		log.info("springdemo-hibernate-启动----------------------------");
 		SpringApplication.run(AaaApplication.class, args);
+//		entitymanager 管理的创建通过jdk动态代理实现；返回的代理对象
+//		SharedEntityManagerBean
+//		public static EntityManager createSharedEntityManager(EntityManagerFactory emf, @Nullable Map<?, ?> properties,
+//				boolean synchronizedWithTransaction, Class<?>... entityManagerInterfaces) {
+//
+//			ClassLoader cl = null;
+//			if (emf instanceof EntityManagerFactoryInfo) {
+//				cl = ((EntityManagerFactoryInfo) emf).getBeanClassLoader();
+//			}
+//			Class<?>[] ifcs = new Class<?>[entityManagerInterfaces.length + 1];
+//			System.arraycopy(entityManagerInterfaces, 0, ifcs, 0, entityManagerInterfaces.length);
+//			ifcs[entityManagerInterfaces.length] = EntityManagerProxy.class;
+//			return (EntityManager) Proxy.newProxyInstance(
+//					(cl != null ? cl : SharedEntityManagerCreator.class.getClassLoader()),
+//					ifcs, new SharedEntityManagerInvocationHandler(emf, properties, synchronizedWithTransaction));
+//		}
+
 	}
+ 	
 }
